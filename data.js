5b4225cd75c74af2bc3ece3fce4adaa100c092bd
jcndongo <mjndongo@gmail.com>
2023-05-30 12:20:10 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,31 @@
+let data = {
+    Afghanistan: {
+        "Social_support": 0.341,
+        "Corruption": 0.847,
+    },
+
+    Lebanon: {
+        "Social_support": 0.530,
+        "Corruption": 0.891,
+    },
+
+    Zimbabwe: {
+        "Social_support": 0.690,
+        "Corruption": 0.766,
+    },
+
+    Botswana: {
+        "Social_support": 0.753,
+        "Corruption": 0.830,
+    },
+
+    Tanzania: {
+        "Social_support": 0.653,
+        "Corruption": 0.554,
+    },
+
+    Zambia: {
+        "Social_support": 0.694,
+        "Corruption": 0.818,
+    },
+};
